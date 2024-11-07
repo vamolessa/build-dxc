1f314a9e0751f152c346788127f7cdde45e40b54
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2024-11-07 19:26:39 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir build
 pushd build
 
 cmake -S .. -B . -C ../cmake/caches/PredefinedParams.cmake -D DCMAKE_BUILD_TYPE=Release
-cmake --build .
+cmake --build . --config Release
 
 popd
 popd
@@ -19,49 +19,31 @@ mkdir dxc-$OS-$ARCH/include
 
 echo $COMMIT > dxc-$OS-$ARCH/commit.txt
 
-cp -f dxc/build/bin/dxc.exe               dxc-$OS-$ARCH/bin
-cp -f dxc/build/bin/dxc                   dxc-$OS-$ARCH/bin
+OUT_DIR=dxc/build
+if [ "$OS" == "win" ]; then
+  OUT_DIR="$OUT_DIR/Release"
+fi
 
-cp -f dxc/build/lib/dxcompiler.dll        dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/dxcompiler.lib        dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/libdxcompiler.dylib   dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/libdxcompiler.so      dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/libdxcompiler.a       dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/bin/dxc.exe"               dxc-$OS-$ARCH/bin
+cp -f "$OUT_DIR/bin/dxc"                   dxc-$OS-$ARCH/bin
 
-cp -f dxc/build/lib/dxil.dll              dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/dxil.lib              dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/libdxil.dylib         dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/libdxil.so            dxc-$OS-$ARCH/lib
-cp -f dxc/build/lib/libdxil.a             dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/dxcompiler.dll"        dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/dxcompiler.lib"        dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/libdxcompiler.dylib"   dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/libdxcompiler.so"      dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/libdxcompiler.a"       dxc-$OS-$ARCH/lib
 
-cp -f dxc/include/dxc/dxcapi.h            dxc-$OS-$ARCH/include
-cp -f dxc/include/dxc/dxcerrors.h         dxc-$OS-$ARCH/include
-cp -f dxc/include/dxc/dxcisense.h         dxc-$OS-$ARCH/include
+cp -f "$OUT_DIR/lib/dxil.dll"              dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/dxil.lib"              dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/libdxil.dylib"         dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/libdxil.so"            dxc-$OS-$ARCH/lib
+cp -f "$OUT_DIR/lib/libdxil.a"             dxc-$OS-$ARCH/lib
+
+cp -f "dxc/include/dxc/dxcapi.h"           dxc-$OS-$ARCH/include
+cp -f "dxc/include/dxc/dxcerrors.h"        dxc-$OS-$ARCH/include
+cp -f "dxc/include/dxc/dxcisense.h"        dxc-$OS-$ARCH/include
 
 cp -f dxc/external/DirectX-Headers/include/directx/d3d12shader.h dxc-$OS-$ARCH/include
-
-echo "======================================================================================"
-echo "====================================================================================== find dxc.exe"
-echo "======================================================================================"
-
-find . -type f -name 'dxc.exe'
-
-echo "======================================================================================"
-echo "====================================================================================== find dxcompiler.dll"
-echo "======================================================================================"
-
-find . -type f -name '*dxcompiler.dll'
-
-echo "======================================================================================"
-echo "====================================================================================== find dxil.dll"
-echo "======================================================================================"
-
-find . -type f -name '*dxil.dll'
-
-echo "======================================================================================"
-echo "======================================================================================"
-echo "======================================================================================"
-
 rm -f dxc-$OS-$ARCH-$BUILD_DATE.zip
 
 if [ "$OS" == "win" ]; then
