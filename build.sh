985c614afa779623b01c6006149551857a15ca10
Matheus Lessa <1696072+vamolessa@users.noreply.github.com>
2024-11-07 18:11:12 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -40,5 +40,6 @@ cp -f dxc/include/dxc/dxcisense.h         dxc-$OS-$ARCH/include
 
 cp -f dxc/external/DirectX-Headers/include/directx/d3d12shader.h dxc-$OS-$ARCH/include
 
-rm -f dxc-$OS-$ARCH-$BUILD_DATE.zip
-zip -9 -r dxc-$OS-$ARCH-$BUILD_DATE.zip dxc-$OS-$ARCH || echo "could not zip artifacts"
+#rm -f dxc-$OS-$ARCH-$BUILD_DATE.zip
+#zip -9 -r dxc-$OS-$ARCH-$BUILD_DATE.zip dxc-$OS-$ARCH || echo "could not zip artifacts"
+7z a -y -mx=9 dxc-$OS-$ARCH-$BUILD_DATE.zip dxc-$OS-$ARCH
